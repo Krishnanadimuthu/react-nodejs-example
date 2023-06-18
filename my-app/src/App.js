2cbeea3014437aa54b17bc94dca4dbb340ecac58
Krishnanadimuthu <vrass88nkk@gmail.com>
2023-06-18 14:46:03 +0530

    bug clear

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import './App.css';
 import { Header } from './components/Header'
 import { Users } from './components/Users'
 import { DisplayBoard } from './components/DisplayBoard'
